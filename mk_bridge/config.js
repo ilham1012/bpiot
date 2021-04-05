279ec35a25895668e319261dfcdec0839a235cd1
ilham1012 <email.ilham@gmail.com>
2021-04-05 16:49:15 +0700

    Add MQTT - Kafka Bridge

---
+++ mk_bridge/config.js
@@ -0,0 +1,90 @@
+"use strict";
+
+module.exports = {
+
+    // mqtt connection options
+    mqtt: { // # see https://github.com/mqttjs/MQTT.js#mqttclientstreambuilder-options
+        url: null,
+        options: {
+            clientId: "example-client",
+            username: "example",
+            password: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjE3NjA5NzU0fQ.NEHbivOZUjkP8z1sL8YxhMNHZAo7zLzR9m46V_3NSQc",
+            host: "broker",
+            port: 1883,
+            protocolId: "MQTT",
+            protocolVersion: 4,
+        }
+    },
+
+    // kafka connection options
+    kafka: { // # see https://github.com/nodefluent/node-sinek/blob/master/lib/librdkafka/README.md
+        logger: undefined,
+        noptions: {
+            //"debug": "all",
+            metadata_broker_list: ["172.20.0.6:9092"],
+            client_id: "mqtt-bridge-example-client",
+            "event_cb": true,
+            "compression.codec": "none",
+            "retry.backoff.ms": 200,
+            "message.send.max.retries": 10,
+            "socket.keepalive.enable": true,
+            "queue.buffering.max.messages": 100000,
+            "queue.buffering.max.ms": 1000,
+            "batch.num.messages": 1000000,
+
+            //"security.protocol": "sasl_ssl",
+            //"ssl.key.location": path.join(__dirname, "../certs/ca-key"),
+            //"ssl.key.password": "nodesinek",
+            //"ssl.certificate.location": path.join(__dirname,"../certs/ca-cert"),
+            //"ssl.ca.location": path.join(__dirname,"../certs/ca-cert"),
+            //"sasl.mechanisms": "PLAIN",
+            //"sasl.username": "admin",
+            //"sasl.password": "nodesinek",
+            "api.version.request": true,
+        },
+        tconf: {
+            "request.required.acks": 1,
+        }
+    },
+
+    // declares on which target kafka topic a mqtt message should be routed to (based on the mqtt topic)
+    routing: {
+
+        //"*": "*", // from all to all (indiviudally 1:1)
+        //"*": "kafka-test", // from all to single kafka-test topic
+        //"mqtt-topic": "kafka-topic", // from mqtt-topic to kafka-topic only
+
+        "*": "mqtt-bridge-example"
+    },
+
+    // if routed messages should be logged to debug
+    logMessages: true,
+
+    // declares how an mqtt topic name should be split (/) to fit to the kafka topic naming conventions
+    kafkaTopicDelimiter: "-",
+
+    // gives you the option to alter mqtt messages before they are consumed (routed)
+    subscribeEtl: (topic, message, packet, callback) => {
+        // first param is an error, if you pass one, we will omit the message
+        callback(null, {
+            topic,
+            message,
+        });
+    },
+
+    // gives you the option to alter kafka messages before they are produced
+    produceEtl: (topic, message, key, callback) => {
+        // first param is an error, if you pass one, we will omit the message
+        callback(null, {
+            topic,
+            message, // you can pass an object, will be turned into a string
+            key, // default uuid.v4
+            partition: null, // default null
+        });
+    },
+
+    // the bridge starts an http server
+    http: {
+        port: 3967,
+    },
+};
---
+++ mk_bridge/lib/kafka/KafkaClient.js
@@ -0,0 +1,103 @@
+"use strict";
+
+const EventEmitter = require("events");
+const Promise = require("bluebird");
+const debug = require("debug")("mqtttokafka:kafka");
+// const { NProducer } = require("sinek");
+const uuid = require("uuid");
+const { Kafka } = require('kafkajs');
+// const config = require("../../../mqtt_kafka/config");
+
+class KafkaClient extends EventEmitter {
+
+    constructor(config = {}, etl = null) {
+        super();
+        
+        this.config = config;
+        this.etl = etl;
+        this.kafka = null;
+        this.producer = null;
+    }
+
+    async connect(){
+        // debug("Connecting..");
+
+        // this.producer = new NProducer(this.config, "auto");
+        // this.producer.on("error", (error) => {
+        //     this.emit("error", error);
+        // });
+
+        // await this.producer.connect();
+        // debug("Connected.");
+        console.log("kafka broker: ", this.config.noptions.metadata_broker_list);
+
+        this.kafka = new Kafka({
+            clientId: this.config.noptions.client_id,
+            brokers: this.config.noptions.metadata_broker_list // ['kafka1:9092', 'kafka2:9092']
+        });
+
+        this.producer = this.kafka.producer();
+
+        await this.producer.connect();
+        
+    }
+
+    produce(_topic, _message){
+
+        const _key = uuid.v4();
+
+        console.log("KafkaClient");
+        console.log("topic: ", _topic);
+        console.log("message: ", _message);
+
+        if(!this.etl){
+
+            if(typeof _message !== "string"){
+                _message = JSON.stringify(_message);
+            }
+
+            // return this.producer.send(_topic, _message, null, _key);
+            return this.producer.send({ topic: _topic, messages: [{ key: key, value: _message }]});
+        }
+
+        return new Promise((resolve, reject) => {
+            
+            this.etl(_topic, _message, _key, (error, result) => {
+                
+                if(error){
+                    return debug("Skipping message of", _topic, "as etl returned error.");
+                }
+
+                let {
+                    topic,
+                    message,
+                    key,
+                    partition = null,
+                } = result;
+
+                if(typeof message !== "string"){
+                    message = JSON.stringify(message);
+                }
+
+                console.log("topic_: ", topic);
+                console.log("message_: ", message);
+                // this.producer.send(topic, message, partition, key).then(resolve).catch(reject);
+                this.producer.send({ topic: topic, messages: [{ key: key, value: message }]}).then(resolve).catch(reject);
+            });
+        });
+    }
+
+    getStats(){
+        return 1; //this.producer.getStats();
+    }
+
+    close(){
+
+        if(this.producer){
+            // this.producer.close();
+            this.producer.disconnect();
+        }
+    }
+}
+
+module.exports = KafkaClient;
