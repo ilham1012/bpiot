d6e627840c5a1f2bbc640a83ef2203e81589e5de
ilham1012 <email.ilham@gmail.com>
2021-04-08 14:48:25 +0700

    Adjust Telemetry Model for saving

--- backend/app/models/index.js
+++ backend/app/models/index.js
@@ -29,42 +29,45 @@ db.acl = require("./acl.model.js")(sequelize, Sequelize);
 db.telemetry = require("./telemetry.model.js")(sequelize, Sequelize);
 db.telemetry_key = require("./telemetry_key.model.js")(sequelize, Sequelize);
 
+// PROJECT 1--n DEVICE 
 db.project.hasMany(db.device, { as: "devices" });
-
 db.device.belongsTo(db.project, {
-  foreignKey: "projectId",
+  foreignKey: "project_id",
   as: "project",
 });
 
+
+// PROJECT n--n USER 
 db.project.belongsToMany(db.user, {
     as: 'users',
     through: "user_projects",
-    // foreignKey: "projectId",
-    // otherKey: "userId"
 });
 
 db.user.belongsToMany(db.project, {
     as: 'projects',
     through: "user_projects",
-    // foreignKey: "userId",
-    // otherKey: "projectId"
 });
 
+// DEVICE 1--n ACL 
 db.device.hasMany(db.acl, {as: 'control_list'});
 db.acl.belongsTo(db.device, {
-    foreignKey: 'deviceId',
+    foreignKey: 'device_id',
     as: 'acl_device'
 });
 
-db.device.hasMany(db.telemetry, {as: 'telemetry'});
+
+// DEVICE 1--n TELEMETRY 
+db.device.hasMany(db.telemetry, {as: 'device_telemetry'});
 db.telemetry.belongsTo(db.device, {
-    foreignKey: 'deviceId',
+    foreignKey: 'device_id',
     as: 'telemetry_device'
 });
 
+
+// TELEMETRY_KEY 1--n TELEMETRY 
 db.telemetry_key.hasMany(db.telemetry, {as: 'key_telemetry'});
 db.telemetry.belongsTo(db.telemetry_key, {
-    foreignKey: 'keyId',
+    foreignKey: 'key_id',
     as: 'telemetry_key'
 });
 
--- backend/app/models/telemetry.model.js
+++ backend/app/models/telemetry.model.js
@@ -1,27 +1,21 @@
 module.exports = (sequelize, Sequelize) => {
     const Telemetry = sequelize.define("telemetries", {
-        time: Sequelize.DATE,
-        // device: {
-        //     type: DataTypes.INTEGER,
-        //     references: {
-        //     model: device,
-        //     key: 'id',
-        //     // deferrable: Deferrable.INITIALLY_IMMEDIATE
-        //     }
-        // },
-        // key: {
-        //     type: DataTypes.INTEGER,
-        //     references: {
-        //     model: telemetry_key,
-        //     key: 'id',
-        //     // deferrable: Deferrable.INITIALLY_IMMEDIATE
-        //     }
-        // },
+        id: {
+            autoIncrement: true,
+            primaryKey: true,
+            type: Sequelize.INTEGER
+        },
+        time: {
+            allowNull: false,
+            primaryKey: true,
+            type: Sequelize.DATE
+        },
         string_val: Sequelize.STRING,
         long_val: Sequelize.BIGINT,
         double_val: Sequelize.DOUBLE,
         json_val: Sequelize.JSON
-    }, {
+    },
+    {
         sequelize,
         modelName: 'telemetry',
         timestamps: false,
