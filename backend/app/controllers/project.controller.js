d8371d4ebf8b3a3f402b273eb7e08dff60007338
ilham1012 <email.ilham@gmail.com>
2021-04-08 18:06:39 +0700

    Add CRUD to project controller

--- backend/app/controllers/project.controller.js
+++ backend/app/controllers/project.controller.js
@@ -6,7 +6,24 @@ const Project = db.project;
 const User = db.user;
 const project = {};
 
-project.create = (project) => {
+project.create = async (project) => {
+    var same_uid = true;
+    var uid = nanoid(10);
+    
+    while(same_uid){
+        uid = nanoid(10);
+        let project = await Project.findOne({where: {project_uid: uid}});
+        
+        console.log("find uid ");
+        console.log(project);
+        if (project == null){
+            same_uid = false;
+        }
+    }
+
+    console.log("PROJECT CREATE");
+    console.log("uid: ", uid);
+
     return Project.create({
         project_uid: nanoid(10),
         name: project.name,
@@ -23,25 +40,10 @@ project.create = (project) => {
   };
 
 
-project.findById = (projectId) => {
-    return Project.findByPk(projectId, 
+project.findById = (project_id) => {
+    return Project.findByPk(project_id, 
         {
-            include: [
-                "devices", "users"
-            ]
-        })
-        .then((project) => {
-            return project;
-        })
-        .catch((err) => {
-            console.log(">> Error while finding project: ", err);
-            return null;
-        });
-};
-
-project.findAll = () => {
-    return Project.findAll({
-        include:
+            include:
             [
                 "devices",
                 {
@@ -53,20 +55,77 @@ project.findAll = () => {
                     }
                 },
             ],
+        })
+        .then((project) => {
+            return project;
+        })
+        .catch((err) => {
+            console.log(">> Error while finding project: ", err);
+            return null;
+        });
+};
+
+project.findAll = function () {
+    return Project.findAll({
+
     }).then((projects) => {
+        console.log("FIND ALL");
+        console.log(projects);
         return projects;
     });
-  };
+};
 
-project.addUser = (projectId, userId) => {
-    return Project.findByPk(projectId)
+
+// Update a Project by the id in the request
+project.update = (id, body) => {
+    const name = body.name;
+    const desc = body.description;
+
+    return Project.update({name: name, description: desc}, {
+      where: { id: id }
+    })
+        .then(num => {
+            if (num == 1) {
+                return {status: 200, message: "Project was updated successfully."};
+            } else {
+                return {status: 404, message: `Cannot update Project with id=${id}. Maybe Project was not found or req.body is empty!`};
+            }
+        })
+    .catch(err => {
+        return {status: 500, message: "Error updating Project with id=" + id};
+    });
+}
+
+
+// Delete a Project with the specified id in the request
+project.delete = (id) => {
+    // const id = req.params.id;
+
+    return Project.destroy({
+        where: { id: id }
+    })
+        .then(num => {
+        if (num == 1) {
+            return {status: 200, message: "Project was deleted successfully!"};
+        } else {
+            return {status: 404, message: `Cannot delete Project with id=${id}. Maybe Project was not found!`};
+        }
+    })
+    .catch(err => {
+        return {status: 500, message: "Could not delete Project with id=" + id};
+    });
+};
+
+
+project.addUser = (project_id, user_id) => {
+    return Project.findByPk(project_id)
       .then((project) => {
         if (!project) {
           console.log("Project not found!");
           return null;
         }
         
-        return User.findByPk(userId).then((user) => {
+        return User.findByPk(user_id).then((user) => {
           if (!user) {
             console.log("User not found!");
             return null;
@@ -80,36 +139,52 @@ project.addUser = (projectId, userId) => {
       .catch((err) => {
         console.log(">> Error while adding User to Project: ", err);
       });
-  };
+};
 
-project.api_create = (req, res) => {
-    newProject = project.create(req.body);
+
+// ========== API ==========
+
+
+project.api_create = async (req, res) => {
+    let newProject = await project.create(req.body);
 
     if (newProject != null){
-        res.status(201, 'Project was created successfully');
+        return res.status(201).send({message: 'Project was created successfully'});
     }
 
-    res.status(500, 'Error while creating project.');
+    return res.status(500).send({message: 'Error while creating project.'});
 }
 
-project.api_findById = (req, res) => {
-    result = project.findById(req.params.id);
+project.api_findById = async (req, res) => {
+    let result = await project.findById(req.params.id);
+    console.log("Project API");
+    console.log(result);
 
     if (result != null){
-        res.status(200, result);
+        return res.status(200).send({message: result});
     }
     
-    res.status(404, 'Project not found.');
+    return res.status(404).send({message: 'Project not found.'});
 }
 
-project.api_findAll = (req, res) => {
-    result = project.findAll();
+project.api_findAll = async (req, res) => {
+    let result = await project.findAll();
 
     if (result != null){
-        res.status(200, result);
+        return res.status(200).send({message: result});
     }
     
-    res.status(404, 'No project found.');
+    return res.status(404).send({message: 'No project found.'});
+}
+
+project.api_update = async(req, res) => {
+    let result = await project.update(req.params.id, req.body);
+    return res.status(result.status).send({message: result.message});
+};
+
+project.api_delete = async(req, res) => {
+    let result = await project.delete(req.params.id);
+    return res.status(result.status).send({message: result.message});
 }
 
 
