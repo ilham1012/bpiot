dca00a9175be921eed510e9b19a3f6305d47c31e
siroj05 <rojudin123@gmail.com>
2021-08-13 13:39:12 +0700

    add update device, regenerate token, form validation, create, update, delete acl

--- frontend/src/views/Tables/AclTable.vue
+++ frontend/src/views/Tables/AclTable.vue
@@ -16,27 +16,116 @@
         </template>
 
         <template v-slot:default="row">
-          <th scope="row">
+          <th scope="row"> 
             {{ row.item.pattern }}
+            
           </th>          
-
           <td class="text-right">
             <base-button
               nativeType="button"
               type="danger"
               size="sm"
-              @click="deleteProject(row.item.id)"
+              @click="deleteAcl(row.item.id)"
             >
               Delete
+            </base-button>
+            <base-button
+              nativeType="button"
+              type="primary"
+              size="sm"
+              @click="showUpdate(row.item.id)"
+            >
+              Update
             </base-button>
           </td>
         </template>
       </base-table>
     </div>
+   <modal v-model:show=" update_acl"
+           body-classes="p-0"
+           modal-classes="modal-dialog-centered modal-sm">
+      <card type="secondary" shadow
+            header-classes="bg-white pb-5"
+            body-classes="px-lg-5 py-lg-5"
+            class="border-0">
 
+       
+          <h3 class="modal-title mb-3 text-center" id="modal-title-default">New ACL</h3>
+        
+        <form role="form" name="form" @submit.prevent="updateAcl">
+          <label for="">MQTT</label>  
+          <label for="">Pattern</label>
+          <div class="radio custom-control custom-radio custom-control-alternative">
+            <div>
+              <input type="radio" name="mqtt" id="pub" value="true" v-model="Acl.pub"> 
+              <label for="pub">Publish</label>
+            </div>
+            <div>
+              <input type="radio" name="mqtt" id="sub" value="false" v-model="Acl.pub">
+              <label for="sub">Subscribe</label>
+            </div>   
+          </div>
+          <div class="custom-control custom-radio custom-control-alternative">  
+          </div>      
+          <base-input formClasses="input-group-alternative mb-3"
+                      placeholder="Publish"
+                      
+                      v-model="Acl.pattern">
+          </base-input>
+          <!-- <base-input formClasses="input-group-alternative mb-3"
+                      placeholder="Number"
+                      type="number"
+                      v-model="device_id">
+          </base-input> -->
+          
+          
+          <div class="text-center">
+            <base-button type="primary" class="my-4" native-type="submit">Update ACL</base-button>
+          </div>
+        </form>
+
+      </card>
+    </modal>
+    <modal v-model:show="delete_acl"
+           ref="modal_delete_device"
+           body-classes="p-0"
+           modal-classes="modal-dialog-centered modal-sm">
+      <card type="secondary" shadow
+            header-classes="bg-white pb-5"
+            body-classes="px-lg-5 py-lg-5"
+            class="border-0">
+      
+          <h3 class="modal-title mb-3" id="modal-title-default">Delete Acl?</h3>
+          <div class="text-center">
+            <base-button
+              nativeType="button"
+              type="danger"
+              size="sm"
+              @click="deleteAcl"
+            >
+              Delete
+            </base-button>
+             <base-button
+              nativeType="button"
+              type="primary"
+              size="sm"
+              @click="cancel"
+            >
+              cancel
+            </base-button>
+          </div>
+          
+          
+      </card>
+    </modal>
   </div>
 </template>
 <script>
+import Acl from '../../models/acl';
+import AclService from '../../services/acl.service';
+import deviceService from '../../services/device.service';
+
+
 export default {
   name: "acl-table",
   props: {
@@ -46,6 +135,111 @@ export default {
     title: String,
     acls: Array,
   },
+  data(){
+    return {
+      id : "",
+      delete_acl : false,
+      update_acl : false,
+      Acl : new Acl("",""),
+    }
+  },
+  methods:{
+    deleteAcl(id) {
+      AclService.delete(id)
+        .then((response) => {
+          console.log(response);
+          this.retrieveAcls()
+         
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    },
+    popUpDelete(id){
+      this.delete_acl = true;
+      this.id = id;
+    },
+    showUpdate(id){
+
+      this.update_acl = true;
+      this.id = id;
+      // AclService.get(id)
+      //   .then(response => {
+      //     this.Acl = response.data;
+      //     console.log(response.data);
+      //   })
+      //   .catch(e => {
+      //     console.log(e);
+      //   });
+      
+    },
+    updateAcl(){
+     
+      console.log("Update Device");
+      console.log(this.Acl);
+      this.Acl.device_id = this.device.id
+      AclService.update(this.id, this.Acl)
+        .then(response => {
+          console.log(response.data);
+
+          console.log("Device Berhasil Diupdate");
+
+       
+          this.update_acl = false;
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    },
+    async retrieveAcls() {
+
+      console.log("retrieve Acl");
+
+      AclService.get(this.$route.params.id)
+        .then((response) => {
+          
+          const acls = response.data;
+          
+          acls.forEach(acl => {
+            console.log(acl)
+            if (acl.pub){
+              this.pub_acls.push(acl);
+            } 
+            else {
+              this.sub_acls.push(acl);
+            }
+          });
+
+          
+
+        })
+        
+        .catch((e) => {
+          
+          console.log(e);
+        });
+    },
+    async retrieveDevice() {
+      console.log("retrieve device");
+      
+      deviceService.get(this.$route.params.id)
+        .then((response) => {
+          this.device = response.data;
+          console.log(this.device);
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    },
+  },
+  mounted() {
+    // this.retrieveNewAcls();
+    this.retrieveDevice()
+      .then(this.retrieveAcls())
+  },
+ 
+  
+    
 };
 </script>
 <style></style>
--- frontend/src/views/Tables/DevicesTable.vue
+++ frontend/src/views/Tables/DevicesTable.vue
@@ -35,9 +35,7 @@
         <template v-slot:default="row">
           <th scope="row">
             <div class="media align-items-center">
-              <!-- <a href="#" class="avatar rounded-circle mr-3"> -->
-                <!-- <img alt="Image placeholder" :src="row.item.img" /> -->
-              <!-- </a> -->
+              
               <div class="media-body">
                 <span class="name mb-0 text-sm">
                   <a v-bind:href="'#' + /device/ + row.item.id">
@@ -60,9 +58,17 @@
           <td class="text-right">
             <base-button
               nativeType="button"
+              type="primary"
+              size="sm"
+              @click="showUpdate(row.item.id)"
+            >
+              Update
+            </base-button>
+            <base-button
+              nativeType="button"
               type="danger"
               size="sm"
-              @click="deleteProject(row.item.id)"
+              @click="popUpDelete(row.item.id)"
             >
               Delete
             </base-button>
@@ -77,7 +83,42 @@
     >
       <base-pagination total="30"></base-pagination>
     </div> -->
+    
+    <modal v-model:show="modals.device_update"
+           body-classes="p-0"
+           modal-classes="modal-dialog-centered modal-sm">
+      <card type="secondary" shadow
+            header-classes="bg-white pb-5"
+            body-classes="px-lg-5 py-lg-5"
+            class="border-0">
 
+        <!-- <template v-slot:header> -->
+          <h3 class="modal-title mb-3" id="modal-title-default">Update Device</h3>
+        <!-- </template> -->
+
+        <form role="form" name="form" @submit.prevent="updateDevice">
+          <select name="" class="form-control input-group-alternative mb-3" v-model="device.project">
+            <option v-for="(item , index) in projects" v-bind:key="index" :selected="index == 0" v-bind:value="item">
+              {{item.name}}
+            </option>
+          </select>          
+          <base-input formClasses="input-group-alternative mb-3"
+                      placeholder="Project Name"
+                      addon-left-icon="ni ni-email-83"
+                      v-model="device.name">
+          </base-input>
+          <textarea class="form-control form-control-alternative"
+                    rows="2"
+                    placeholder="Project Description"
+                    v-model="device.description">
+          </textarea>
+          <div class="text-center">
+            <base-button type="primary" class="my-4" native-type="submit">Update Project</base-button>
+          </div>
+        </form>
+
+      </card>
+    </modal>
     <modal v-model:show="modals.modal_token"
           ref="modal_token"
           body-classes="p-0"
@@ -100,14 +141,47 @@
       </template>
       </card>
     </modal>
-
+    <modal v-model:show="modals.delete_device"
+           ref="modal_delete_device"
+           body-classes="p-0"
+           modal-classes="modal-dialog-centered modal-sm">
+      <card type="secondary" shadow
+            header-classes="bg-white pb-5"
+            body-classes="px-lg-5 py-lg-5"
+            class="border-0">
+      
+          <h3 class="modal-title mb-3" id="modal-title-default">Delete Device?</h3>
+          <div class="text-center">
+            <base-button
+              nativeType="button"
+              type="danger"
+              size="sm"
+              @click="deleteDevice"
+            >
+              Delete
+            </base-button>
+             <base-button
+              nativeType="button"
+              type="primary"
+              size="sm"
+              @click="cancel"
+            >
+              cancel
+            </base-button>
+          </div>
+          
+          
+      </card>
+    </modal>
   </div>
 </template>
 <script>
+import BaseButton from '../../components/BaseButton.vue';
 import Device from "../../models/device";
 import DeviceService from '../../services/device.service';
-
+import ProjectService from "../../services/project.service";
 export default {
+  components: { BaseButton },
   name: "devices-table",
   props: {
     type: {
@@ -118,35 +192,37 @@ export default {
   data() {
     return {
       devices: [],
+      projects: [],
       currentDevice: new Device('',''),
       modals: {
-        modal_token: false
+        modal_token: false,
+        device_update:false,
+        delete_device:false,
+
       },
-      // currentProject: null,
-      // currentIndex: -1,
-      // title: "",
-      //   {
-      //     id: 1,
-      //     name: "Coffee Roaster",
-      //     uid: "FFlBjQXwbb",
-      //     description: "Smart Coffee Roaster Project w/ Subang",
-      //   },
-      //   {
-      //     id: 2,
-      //     name: "Water Quality",
-      //     uid: "7f55FFlBjQ",
-      //     description: "Water Quality monitoring",
-      //   },
-      //   {
-      //     id: 3,
-      //     name: "Smart Health",
-      //     uid: "BjQ7f55FFl",
-      //     description: "Smart Health: monitoring",
-      //   },
-      // ],
+      device: new Device("",""),
+      id:""
+     
     };
   },
   methods: {
+    showUpdate(id){
+      this.modals.device_update = true;
+      this.id = id;
+
+      DeviceService.get(id)
+        .then(response => {
+          this.device = response.data;
+          console.log(response.data);
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    },
+    popUpDelete(id){
+      this.modals.delete_device = true;
+      this.id = id;
+    },
     retrieveDevices() {
       console.log("retrieve device");
 
@@ -159,25 +235,45 @@ export default {
           console.log(e);
         });
     },
-    refreshList() {
-      this.retrieveDevices();
-      this.currentDevice = null;
-      this.currentIndex = -1;
-    },
-    deleteProject(id) {
-      DeviceService.delete(id)
+    retrieveProjects() {
+      console.log("retrieve proj");
+
+      ProjectService.getAll()
         .then((response) => {
-          console.log(response);
-          this.retrieveDevices();
+          this.projects = response.data;
+          console.log(response.data);
         })
         .catch((e) => {
           console.log(e);
         });
     },
+
+    refreshList() {
+      this.retrieveDevices();
+      this.currentDevice = null;
+      this.currentIndex = -1;
+    },
+    deleteDevice() {
+      DeviceService.delete(this.id)
+        .then((response) => {
+          console.log(response);
+         
+          this.retrieveDevices();
+          this.modals.delete_device = false;
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    },
+    cancel(id){
+      this.modals.delete_device = false;
+      this.id = id;
+    },
     showTokenModal(device){
       this.currentDevice = device;
       this.modals.modal_token = true;
     },
+    
     generateToken(device){
       DeviceService.generateToken(device)
         .then((response) => {
@@ -188,10 +284,27 @@ export default {
         .catch((e) => {
           console.log(e);
         });
+    },
+
+    updateDevice(){
+      console.log("Update Device");
+      console.log(this.device);
+      DeviceService.update(this.id, this.device)
+        .then(response => {
+          console.log(response.data);
+          console.log("Device Berhasil Diupdate");
+          this.retrieveDevices();
+          this.modals.device_update = false;
+        })
+        .catch(e => {
+          console.log(e);
+        });
     }
+  
   },
   mounted() {
     this.retrieveDevices();
+    this.retrieveProjects();
   },
 };
 </script>
